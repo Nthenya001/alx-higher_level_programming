aa11730a5bd210746ba1fb68b24f2b3ec7958b89
Faith Jacob <faithjack001@gmail.com>
2022-09-27 11:40:47 -0700

    Replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -8,5 +8,6 @@ def replace_in_list(my_list, idx, element):
     if idx > length - 1:
     return (my_list)
 
-my_list[idx] = element
-return (my_list)
+    my_list[idx] = element
+
+    return (my_list)
