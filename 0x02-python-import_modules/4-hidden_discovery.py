23d36e8ca5abc5c0028af79e765031266779b2ba
Faith Jacob <faithjack001@gmail.com>
2022-09-28 10:38:02 -0700

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    length = len(my_list)
-
-    new_list = my_list[:]
-
-
-    if 0 <= idx < length:
-        new_list[idx] = element
-
-    return (new_list)
+if __name__ == "__main__":
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
+            print(name)
